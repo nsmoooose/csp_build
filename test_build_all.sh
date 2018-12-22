11e846b1a367239b38c132b02c522363626e5f5d
Henrik Nilsson <nsmoooose@gmail.com>
2018-12-22 16:15:12 +0100

    Output test build results into its own directory.
    Test results from builds will be put into the build_result directory
    with one file per platform.

--- test_build_all.sh
+++ test_build_all.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 set -e
+
+[ -d build_result ] || mkdir build_result
+rm build_result/*
+
 platforms=(csp:ubuntu_18.04 csp:debian_9.6 csp:fedora_28 csp:fedora_29)
 for platform in ${platforms[@]}; do
 	echo "TEST BUILD ON $platform"
-	docker run --rm -it -v $(pwd)/../:/code/project $platform /code/configure
-	docker run --rm -it -v $(pwd)/../:/code/project $platform /code/clean
-	docker run --rm -it -v $(pwd)/../:/code/project $platform /code/build
-	docker run --rm -it -v $(pwd)/../:/code/project $platform /code/test_build
+	{
+		docker run --rm -it -v $(pwd)/../:/code/project $platform /code/configure
+		docker run --rm -it -v $(pwd)/../:/code/project $platform /code/clean
+		docker run --rm -it -v $(pwd)/../:/code/project $platform /code/build
+		docker run --rm -it -v $(pwd)/../:/code/project $platform /code/test_build
+	} >> build_result/$platform.txt 2>&1
 done
